06f4da63ab1f6094f47323431ca889e612f1c553
Jakub Slawecki <jakubslawecki@gmail.com>
2018-06-11 22:55:59 +0200

    custom view classes added

---
+++ kindOfGymApp/View/TextField.swift
@@ -0,0 +1,22 @@
+//
+//  TextField.swift
+//  kindOfGymApp
+//
+//  Created by Jakub Slawecki on 11.06.2018.
+//  Copyright © 2018 Jakub Slawecki. All rights reserved.
+//
+
+import UIKit
+
+class TextField: UITextField {
+
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+
+}
