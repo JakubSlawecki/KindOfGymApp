80581c8e9809c1a9d8cb6eff818b515f3a6a1f7b
Jakub Slawecki <jakubslawecki@gmail.com>
2018-06-11 23:48:49 +0200

    login with email and password added

--- kindOfGymApp/Controller/LoginVC.swift
+++ kindOfGymApp/Controller/LoginVC.swift
@@ -7,13 +7,16 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginVC: UIViewController {
     @IBOutlet weak var gifView: UIImageView!  //outlet for login background Animation
+    @IBOutlet weak var emailTextField: TextField!
+    @IBOutlet weak var passwordTextField: TextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         gifView.loadGif(name: "LoginBackround") // animation in the background
         
         
@@ -21,12 +24,45 @@ class LoginVC: UIViewController {
         
         
     }
+    
+    
+//    func firebaseAuth(_ credential: AuthCredential) {
+//
+//        Auth.auth().signIn(with: credential, completion: { (user, error) in
+//            if error != nil {
+//
+//                print("Jakub: Unable to auth with Firebase -\(String(describing: error))")
+//            } else {
+//
+//                print("Jakub: Successfully authenticated with Firebase")
+//                if let user = user {
+//
+//                    let userData = ["provider": credential.provider]
+//                    //self.completeSignIN(id: user.uid, userData: userData) // it saves id to keychain
+//                }
+//            }
+//        })
+//    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+
+    @IBAction func signInPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, email != "", let password = passwordTextField.text, password != "" {
+            Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+                if error == nil {
+                    print("Jakub: Email User authenticated with Firebase")
+                } else {
+                    Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if error != nil {
+                            print("Jakub: Unable to authenticate with Firebase using email")
+                        } else {
+                            print("Jakub: Successfully authenticated with Firebase")
+                        }
+                    })
+                }
+            })
+        }
     }
-
-
+    
 }
 
